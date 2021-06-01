ac2900fced4cb7b110384da9a11853660575c438
soundwanders <soundwanders@gmail.com>
2021-05-31 20:52:05 -0400

    whitespace edits

--- part1/src/App.js
+++ part1/src/App.js
@@ -9,6 +9,7 @@ const App = () => {
     );
   };
 
+
   const Part = ({part}) => {
     return (
       <p>
@@ -16,6 +17,7 @@ const App = () => {
       </p>
     );
   };
+
 
   const Content = ({parts}) => {
     return (
@@ -27,37 +29,38 @@ const App = () => {
     );
   };
 
+
   const Total = ({parts}) => {
       return (
           <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
       );
   };
 
-      const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      };
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
-      return (
-          <div>
-              <Header course={course.name} />
-              <Content parts={course.parts} />
-              <Total parts={course.parts} />
-          </div>
-      );
+  return (
+      <div>
+          <Header course={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+      </div>
+  );
 };
 
 export default App;
