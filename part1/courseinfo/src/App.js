ef2c89868abc9c5a1d035f34c46f4f5aae37bbd7
soundwanders <soundwanders@gmail.com>
2021-06-01 15:59:19 -0400

    courseinfo page submission

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,7 +9,7 @@ const Header = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.name} {props.excercises}
+      {props.name} {props.exercises}
     </p>
   )
 };
@@ -17,9 +17,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name = {props.parts[0].name} excercises={props.parts[0].exercises} />
-      <Part name = {props.parts[1].name} excercises={props.parts[1].exercises} />
-      <Part name = {props.parts[2].name} excercises={props.parts[2].exercises} />
+      <Part name = {props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name = {props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name = {props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   )
 };
